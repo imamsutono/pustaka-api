6e676e50752fd22af7257a650f86b62bcbf87905
Imam Sutono <imamsutono>
2022-08-12 16:59:54 +0700

    add param and query handler

--- main.go
+++ main.go
@@ -9,8 +9,30 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/", rootHandler)
+	router.GET("/books/:category/:id", paramHandler)
+	router.GET("/product", queryHandler)
 
 	router.Run()
+}
+
+func queryHandler(context *gin.Context) {
+	category := context.Query("category")
+	id := context.Query("id")
+
+	context.JSON(http.StatusOK, gin.H{
+		"id":       id,
+		"category": category,
+	})
+}
+
+func paramHandler(context *gin.Context) {
+	category := context.Param("category")
+	id := context.Param("id")
+
+	context.JSON(http.StatusOK, gin.H{
+		"id":       id,
+		"category": category,
+	})
 }
 
 func rootHandler(context *gin.Context) {
