aead32cfb047beb713ada3e1164f04f045374e32
Imam Sutono <imamsutono>
2022-08-12 16:26:32 +0700

    add basic routing

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"title": "Hello World!",
+			"bio":   "Lets learn Golang",
+		})
+	})
+
+	router.Run()
+}
