3a6e5ccb1ff75a2c0a7e0dd5694e0e5238458054
Imam Sutono <imamsutono>
2022-08-22 14:42:47 +0700

    add post handler

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 )
 
@@ -12,7 +13,30 @@ func main() {
 	router.GET("/books/:category/:id", paramHandler)
 	router.GET("/product", queryHandler)
 
+	router.POST("/books", postBookHandler)
+
 	router.Run()
+}
+
+type Book struct {
+	Title    string
+	SubTitle string `json:"sub_title"`
+	Price    int
+}
+
+func postBookHandler(context *gin.Context) {
+	var book Book
+
+	err := context.ShouldBindJSON(&book)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"title":     book.Title,
+		"sub_title": book.SubTitle,
+		"price":     book.Price,
+	})
 }
 
 func queryHandler(context *gin.Context) {
