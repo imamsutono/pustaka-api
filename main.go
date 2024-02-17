4faea2eed87cc539c3de2076b928b5b080094187
Imam Sutono <imamsutono>
2024-02-17 20:51:56 +0700

    get gorm and automigration

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"log"
+	"pustaka-api/book"
 	"pustaka-api/handler"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/pustaka_api?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Database connection error!")
+	}
+
+	db.AutoMigrate(&book.Book{})
+
 	router := gin.Default()
 
 	v1 := router.Group("/v1")
